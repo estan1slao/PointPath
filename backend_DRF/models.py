3f86b401874845098bbe8749a01526eb7880fb72
Попова Ксения <ksupop65@mail.ru>
2023-11-22 02:00:42 +0500

    make_bd

--- backend_DRF/models.py
+++ backend_DRF/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    student_id = models.IntegerField(primary_key=True, unique=True)
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    patronymic = models.CharField(blank=True, max_length=20)
+    phone_number = models.TextField(blank=True, max_length=12)
+    personal_email = models.EmailField(max_length=30)
+    grade = models.CharField(max_length=4)
+
+    def __str__(self):
+        return f"{self.surname} {self.name}"
+
+
+class Teacher(models.Model):
+    teacher_id = models.IntegerField(primary_key=True, unique=True)
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    patronymic = models.CharField(blank=True, max_length=20)
+    phone_number = models.TextField(blank=True, max_length=12)
+    personal_email = models.EmailField(max_length=30)
+    discipline = models.TextField()
+
+    def __str__(self):
+        return f"{self.surname} {self.name}"
+
+
+class Project(models.Model):
+    project_id = models.IntegerField(primary_key=True, unique=True)
+    topic = models.TextField()
+    student = models.ForeignKey('Student', on_delete=models.PROTECT)
+    teacher = models.ForeignKey('Teacher', on_delete=models.PROTECT)
+    material_link = models.URLField()
+
+    def __str__(self):
+        return self.topic
+
+
+class Account(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
+    role = models.CharField(max_length=20)
+    login = models.TextField(unique=True)
+    password = models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"Аккаунт пользователя {self.id}"
