882d749ee54dcf9442a6bae13d807707104a709e
estan1slao <stas_latypov@mail.ru>
2023-12-23 03:00:27 +0500

    Add information for Account from Student

--- backend_DRF/models.py
+++ backend_DRF/models.py
@@ -3,31 +3,43 @@ from django.db import models
 from django.conf import settings
 
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 User = settings.AUTH_USER_MODEL
 
 
 class Student(models.Model):
-    student_id = models.IntegerField(primary_key=True, unique=True)
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
-    patronymic = models.CharField(blank=True, max_length=20)
-    phone_number = models.TextField(blank=True, max_length=12) #CharField
-    personal_email = models.EmailField(max_length=30)
-    grade = models.CharField(max_length=4) #blank=True
-    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE, blank=True)
+    #student_id = models.IntegerField(primary_key=True, unique=True)
+    #name = models.CharField(max_length=20)
+    #surname = models.CharField(max_length=20)
+    #patronymic = models.CharField(blank=True, max_length=20)
+    phone_number = models.TextField(blank=True, max_length=12, null=True) #CharField
+    #personal_email = models.EmailField(max_length=30)
+    grade = models.CharField(max_length=4, null=True) #blank=True
+    user = models.OneToOneField(User, verbose_name='Пользователь', on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return f"{self.surname} {self.name}"
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Student.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.student.save()
+
 
 class Teacher(models.Model):
-    teacher_id = models.IntegerField(primary_key=True, unique=True)
-    name = models.CharField(max_length=20)
-    surname = models.CharField(max_length=20)
-    patronymic = models.CharField(blank=True, max_length=20)
-    phone_number = models.TextField(blank=True, max_length=12) #CharField
-    personal_email = models.EmailField(max_length=30)
-    discipline = models.TextField() #blank=True
+    #teacher_id = models.IntegerField(primary_key=True, unique=True)
+    #name = models.CharField(max_length=20)
+    #surname = models.CharField(max_length=20)
+    #patronymic = models.CharField(blank=True, max_length=20)
+    phone_number = models.TextField(blank=True, max_length=12, null=True) #CharField
+    #personal_email = models.EmailField(max_length=30)
+    discipline = models.TextField(null=True) #blank=True
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
