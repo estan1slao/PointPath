04029767cef595736ca15dc349169d55524e2930
Alexander <dayboval_official@mail.ru>
2023-12-28 19:20:38 +0500

    реализован сценарий предложения проекта учеником

--- frontend_react/public/js/project-page.js
+++ frontend_react/public/js/project-page.js
@@ -15,9 +15,32 @@ about.textContent = data.get('about');
 
 const projId = data.get('id');
 
+const URL_DENY = `http://127.0.0.1:8000/projects/teacher-denied-project/${projId}/`;
+const URL_ACCEPT = `http://127.0.0.1:8000/projects/teacher-accept-project/${projId}/`;
+
 const projState = data.get('state');
+const studentId = data.get('studentId');
+
 const takeButton = document.querySelector('.student-choice');
 const trajectoryButton = document.querySelector('.trajectory');
+const approveButton = document.querySelector('.approve');
+const rejectButton = document.querySelector('.reject');
+
+function getTokens () {
+    const cookies = document.cookie.split('; ');
+
+    cookies.forEach((token) => {
+        const [name, value] = token.split('=');
+        if (name === 'access') {
+            tokens.access = value;
+        } else if (name === 'refresh') {
+            tokens.refresh = value;
+        }
+    })
+}
+
+const tokens = {};
+getTokens();
 
 if (projState === '1') {
     takeButton.classList.add('hidden');
@@ -25,9 +48,20 @@ if (projState === '1') {
     trajectoryButton.addEventListener('click', () => {
         window.location.href = `./trajectory.html?${data}`;
     });
+} else if (projState === '0' && studentId !== null) {
+    takeButton.classList.add('hidden');
+    approveButton.classList.remove('hidden');
+    rejectButton.classList.remove('hidden');
+
+    approveButton.addEventListener('click', () => {
+        acceptProject(URL_ACCEPT, tokens.access, onSuccessAccept)
+    });
+    
+    rejectButton.addEventListener('click', () => {
+        console.log(tokens);
+        denyProject(URL_DENY, tokens.access, onSuccessDeny);
+    });
 }
-
-
 
 const URL_TAKE_PROJ = `http://127.0.0.1:8000/projects/student-choose-project/${projId}/`;
 
@@ -52,22 +86,6 @@ function postDataProj (url, token, onSuccess) {
         console.log('Ошибка 2');
     });
 }
-
-function getTokens () {
-    const cookies = document.cookie.split('; ');
-
-    cookies.forEach((token) => {
-        const [name, value] = token.split('=');
-        if (name === 'access') {
-            tokens.access = value;
-        } else if (name === 'refresh') {
-            tokens.refresh = value;
-        }
-    })
-}
-
-const tokens = {};
-getTokens();
 
 takeButton.addEventListener('click', () => {
     postDataProj(URL_TAKE_PROJ, tokens.access, console.log);
@@ -125,4 +143,80 @@ function fillData (data) {
 
         fi.textContent = `${data.last_name} ${data.first_name} ${data.patronymic}`;
     }
+}
+
+// принять/отклонить проект
+
+const approvePopup = document.querySelector('.approve-form').closest('.popup');;
+const deniedPopup = document.querySelector('.denied-form').closest('.popup');;
+
+const closeApproveButton = approvePopup.querySelector('.close-btn');
+const closeDeniedButton = deniedPopup.querySelector('.close-btn');
+
+function acceptProject (url, token, onSuccess) {
+    fetch(url,
+    {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+          },
+    },
+    )
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            console.log('Ошибка 1');
+        }
+    })
+    .then((result) => {
+        onSuccess(result);
+    })
+    .catch(() => {
+        console.log('Ошибка 2');
+    });
+}
+
+function onSuccessAccept (res) {
+    console.log(res);
+
+    closeApproveButton.addEventListener('click', () => {
+        window.location.href = "./consideration-projects.html";
+    });
+}
+
+function denyProject (url, token, onSuccess) {
+    fetch(url,
+    {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+          },
+    },
+    )
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            console.log('Ошибка 1');
+        }
+    })
+    .then((result) => {
+        onSuccess(result);
+    })
+    .catch(() => {
+        console.log('Ошибка 2');
+    });
+}
+
+function onSuccessDeny (res) {
+    console.log(res);
+
+    closeDeniedButton.addEventListener('click', () => {
+        window.location.href = "./consideration-projects.html";
+    });
+
+    
 }
