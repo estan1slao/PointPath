be394cba0b5c1d07e720f6a6062f6ccd5d5ca0d4
Alexander <dayboval_official@mail.ru>
2024-01-09 22:03:33 +0500

    change URLs

--- frontend_react/public/js/modules/urls.js
+++ frontend_react/public/js/modules/urls.js
@@ -1,24 +1,24 @@
-const URL_REG = 'http://127.0.0.1:8000/register/';
-const URL_LOGIN = 'http://127.0.0.1:8000/token/';
-const URL_PROFILE = 'http://127.0.0.1:8000/profile/';
-const URL_CUR_PROJ = "http://127.0.0.1:8000/projects/get-active/";
-const URL_EDIT = 'http://127.0.0.1:8000/profile/update/';
-const URL_PROJECTS = 'http://127.0.0.1:8000/projects/student-get-projects/';
+const URL_REG = 'http://185.199.11.193/register/';
+const URL_LOGIN = 'http://185.199.11.193/token/';
+const URL_PROFILE = 'http://185.199.11.193/profile/';
+const URL_CUR_PROJ = "http://185.199.11.193/projects/get-active/";
+const URL_EDIT = 'http://185.199.11.193/profile/update/';
+const URL_PROJECTS = 'http://185.199.11.193/projects/student-get-projects/';
 
-const URL_DENY_PROJ = 'http://127.0.0.1:8000/projects/teacher-denied-project/'; // + id проекта
-const URL_ACCEPT_PROJ = 'http://127.0.0.1:8000/projects/teacher-accept-project/'; // + id проекта
-const URL_TAKE_PROJ = 'http://127.0.0.1:8000/projects/student-choose-project/'; // + id проекта
+const URL_DENY_PROJ = 'http://185.199.11.193/projects/teacher-denied-project/'; // + id проекта
+const URL_ACCEPT_PROJ = 'http://185.199.11.193/projects/teacher-accept-project/'; // + id проекта
+const URL_TAKE_PROJ = 'http://185.199.11.193/projects/student-choose-project/'; // + id проекта
 
-const URL_TEACHERS = 'http://127.0.0.1:8000/about-teacher/all/';
-const URL_PROPOSE_PROJ_TEACHER = "http://127.0.0.1:8000/projects/teacher-offers-project/";
-const URL_PROPOSE_PROJ_STUDENT = "http://127.0.0.1:8000/projects/student-offers-project/";
-const URL_STUDENT_PROJECTS = "http://127.0.0.1:8000/projects/teacher-viewing-proposed-projects/";
+const URL_TEACHERS = 'http://185.199.11.193/about-teacher/all/';
+const URL_PROPOSE_PROJ_TEACHER = "http://185.199.11.193/projects/teacher-offers-project/";
+const URL_PROPOSE_PROJ_STUDENT = "http://185.199.11.193/projects/student-offers-project/";
+const URL_STUDENT_PROJECTS = "http://185.199.11.193/projects/teacher-viewing-proposed-projects/";
 
-const URL_GET_CARDS = `http://127.0.0.1:8000/getcards/`; // + id проекта
-const URL_OPEN_CARD = 'http://127.0.0.1:8000/card/';
+const URL_GET_CARDS = `http://185.199.11.193/getcards/`; // + id проекта
+const URL_OPEN_CARD = 'http://185.199.11.193/card/';
 
-const URL_GET_COMMENTS = 'http://127.0.0.1:8000/comments/';
-const URL_CREATE_COMMENT = 'http://127.0.0.1:8000/comments/create/';
+const URL_GET_COMMENTS = 'http://185.199.11.193/comments/';
+const URL_CREATE_COMMENT = 'http://185.199.11.193/comments/create/';
 
 export {
     URL_LOGIN, URL_REG, URL_PROFILE, URL_CUR_PROJ, 
